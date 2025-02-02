07c2a362b692ade3c4a514e88787c0221d2c1dc7
Jonas Bak Phillipson <jbakchr@gmail.com>
2025-02-02 22:56:58 +0100

    Added code for making a text button

--- make-text-button/src/main.js
+++ make-text-button/src/main.js
@@ -1,5 +1,19 @@
 import kaplay from "kaplay";
-// import "kaplay/global"; // uncomment if you want to use without the k. prefix
 
-const k = kaplay();
+const k = kaplay({
+  background: [0, 255, 0], // Sætter baggrunden til en grøn farve
+});
 
+// Først laves selve knappen - en rød knap med sorte runde hjørner
+// placeret i midten af skærmen
+const btn = k.add([
+  k.rect(200, 100, { radius: 10 }),
+  k.pos(k.center()),
+  k.anchor("center"),
+  k.area(),
+  k.outline(6),
+  k.color(255, 0, 0),
+]);
+
+// Derefter tilføjes den tekst som skal vises på knappen
+btn.add([k.text("Click me", { size: 30 }), k.anchor("center")]);
